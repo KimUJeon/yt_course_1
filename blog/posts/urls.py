be39811075bfceae82d2b105a1ac253a75c8fd9e
KimUJeon <kinfe_411@naver.com>
2025-02-27 15:19:18 +0900

    init upload

---
+++ blog/posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("home/", views.home),
+    path("<int:id>/", views.post),
+    path("<str:local_name>/", views.local)
+]
---
+++ blog/posts/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+# Create your views here.
+
+posts = [
+    {"id": 1,
+     "title": "one",
+     "content": "one111"},
+    {"id": 2,
+         "title": "two",
+         "content": "two22"},
+    {"id": 3,
+         "title": "three",
+         "content": "three333"},
+]
+
+def home(request):
+    html = ""
+    for post in posts:
+        html += f'''
+            <div>
+        <a href="/post/{post['id']}/">
+            <h1>{post['id']} - {post['title']}</h1> 
+        </a>
+            <p>{post['content']}</p>
+        </div>  
+        '''
+
+    return HttpResponse(html)
+
+def post(request, id):
+    valid_id = False
+
+    for post in posts:
+        if post['id'] == id:
+            post_dict = post
+            valid_id = True
+            break
+    if valid_id:
+        html = f'''
+            <h1>{post_dict['title']}</h1>
+            <p>{post_dict['content']}</p>
+        '''
+
+        return HttpResponse(html)
+
+    else:
+        return HttpResponseNotFound("Post not found")
+
+def local(request, local_name):
+    return HttpResponse(f"<h1>{local_name}</h1>")
+
+def google(request, id):
+    return HttpResponseRedirect(f"/post/{id}/")
